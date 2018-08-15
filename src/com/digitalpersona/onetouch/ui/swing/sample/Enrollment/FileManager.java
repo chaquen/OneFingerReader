5066e3c1132c5d597facd1b2ed16ae2af294f36e
chaquen <edgar.guzman21@gmail.com>
2018-08-15 10:55:37 -0500

    Se agregan campos
    Se agregan campos de validacion para emitir mensaje de error y registro
    de los mismos, se crea actualizacion

--- src/com/digitalpersona/onetouch/ui/swing/sample/Enrollment/FileManager.java
+++ src/com/digitalpersona/onetouch/ui/swing/sample/Enrollment/FileManager.java
@@ -2,31 +2,43 @@ package com.digitalpersona.onetouch.ui.swing.sample.Enrollment;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class FileManager  
 {
-	String dir_archivo;
+	String dir_archivo="logs/logs.txt";
 	
-	public FileManager(String strRuta){
-		this.dir_archivo=strRuta;
-		File archivo = new File(strRuta);
+	public FileManager(){
+		
+		File archivo = new File(this.dir_archivo);
 		
 	}
     	
 	public Boolean Escribir(String txt){
-	   String fin = System.getProperty( "line.separator" );
-	   java.util.Date fecha = new Date();
-	   Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-           String fec = formatter.format(fecha); 	
-	   FileWriter escribir = new FileWriter(this.dir_archivo);	
-	   String texto=" -"+fec+"- {"+texto+"}- "+fin;
-	
-		for(int i=0; i<texto.length();i++){
-	            escribir.write(texto.charAt(i));
-		}
+           try{
+               String fin = System.getProperty( "line.separator" );
+                java.util.Date fecha = new Date();
+                Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String fec = formatter.format(fecha); 	
+                FileWriter escribir = new FileWriter(this.dir_archivo,true);	
+                String texto="";
+                texto+=" -"+fec+"- {"+txt+"}- "+fin;
 
-		escribir.close();
-		return true;
+                     for(int i=0; i<texto.length();i++){
+                         escribir.write(texto.charAt(i));
+                     }
+
+                     escribir.close();
+                     return true;
+           }catch(IOException iex){
+               System.err.println("Error al guardar dato los datos de la huella."+ iex.getMessage());
+               return false;
+           } 
+            
+	   
 	
 	} 
 
--- src/com/digitalpersona/onetouch/ui/swing/sample/Enrollment/VerificationForm.java
+++ src/com/digitalpersona/onetouch/ui/swing/sample/Enrollment/VerificationForm.java
@@ -4,45 +4,29 @@ import com.digitalpersona.onetouch.*;
 import com.digitalpersona.onetouch.verification.*;
 import com.mysql.jdbc.exceptions.MySQLDataException;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.DateFormat;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JButton;
 import javax.swing.JOptionPane;
-import java.util.Calendar;
 
 public class VerificationForm extends CaptureForm 
 {
-	private DPFPVerification verificator = DPFPGlobal.getVerificationFactory().createVerification();
-                //Esta variable tambien captura una huella del lector y crea sus caracteristcas para auntetificarla
+	//Esta variable tambien captura una huella del lector y crea sus caracteristcas para auntetificarla
         // o verificarla con alguna guardada en la BD
         private DPFPVerification Verificador = DPFPGlobal.getVerificationFactory().createVerification();
-        
-        
+                
         private DPFPTemplate template;
         
         public DPFPFeatureSet featuresverificacion;
         
         public static String TEMPLATE_PROPERTY = "template";
         
-      
-         
 	VerificationForm(Frame owner) {
-            
-		super(owner);
-          
-                    
+          super(owner);
         }
     
 	@Override protected void init()
@@ -50,9 +34,7 @@ public class VerificationForm extends CaptureForm
 		super.init();
 		this.setTitle("Verifica tu huella");
 		updateStatus(0);
-                
-                
-	}
+        }
 
 	
         @Override protected void process(DPFPSample sample) {
@@ -68,9 +50,6 @@ public class VerificationForm extends CaptureForm
                     id_e= rs2.getInt("id");
                     
                 }
-                
-                
-                
                 DPFPFeatureSet features = extractFeatures(sample, DPFPDataPurpose.DATA_PURPOSE_VERIFICATION);
 
                 Connection c=con.conectar();
@@ -102,8 +81,7 @@ public class VerificationForm extends CaptureForm
                         //e indica el nombre de la persona que coincidió.
                         if (result.isVerified()){
                             String nombre=rs.getString("nombre");
-                            id_u=rs.getInt("documento");
-                           
+                            id_u=rs.getInt("documento");                           
                             String msnAdi="";
                             //crea la imagen de los datos guardado de las huellas guardadas en la base de datos
                             actualizarHuella(id_u,id_e,nombre);
@@ -111,73 +89,60 @@ public class VerificationForm extends CaptureForm
                             return;
                         }else{
                             no_existe=true;
-                        }
-                        
+                        }                       
                       
                 }
                 if(no_existe){
                     JOptionPane.showMessageDialog(null, "Este usuario no aparece registrado, por favor vaya a la opcion registrar huella","Verificacion de Huella", JOptionPane.INFORMATION_MESSAGE);
                 }
-                //fin while
-                    
-                   
-                
-                // Process the sample and create a feature set for the enrollment purpose.
+                //fin while                
             } catch (SQLException ex) {
                 Logger.getLogger(VerificationForm.class.getName()).log(Level.SEVERE, null, ex);
+                FileManager fl= new FileManager();
+                fl.Escribir("SQLException: "+ ex.getMessage());
+                JOptionPane.showMessageDialog(null,"Se ha generado un error por favor intenta la verificación de nuevo, si la falla persiste comunicate con el administrador de el sistema","Registro de Huella", JOptionPane.INFORMATION_MESSAGE);
+            } catch (IOException ex) {
+                Logger.getLogger(VerificationForm.class.getName()).log(Level.SEVERE, null, ex);
+                FileManager fl= new FileManager();
+                fl.Escribir("IOException: "+ ex.getMessage());
+                JOptionPane.showMessageDialog(null,"Se ha generado un error por favor intenta la verificación de nuevo, si la falla persiste comunicate con el administrador de el sistema","Registro de Huella", JOptionPane.INFORMATION_MESSAGE);
             }
 		
 	}
 	
-	private void updateStatus(int FAR)
-	{
-		// Show "False accept rate" value
-		setStatus(String.format("(FAR) = %1$s", FAR));
-	}
-    ConexionBD con=new ConexionBD(); 
+  private void updateStatus(int FAR){
+        // Show "False accept rate" value
+	setStatus(String.format("(FAR) = %1$s", FAR));
+  }
+  ConexionBD con=new ConexionBD(); 
  
-  
-  
   public DPFPTemplate getTemplate() {
         return template;
-    }
-
+  }
   public void setTemplate(DPFPTemplate template) {
         DPFPTemplate old = this.template;
 	this.template = template;
 	firePropertyChange(TEMPLATE_PROPERTY, old, template);
-  }
-  
-  
-  
-   public void actualizarHuella(int id_u,int id_e,String nombre){
+  }  
+  public void actualizarHuella(int id_u,int id_e,String nombre) throws IOException{
      //Obtiene los datos del template de la huella actual
-     
-
     //Pregunta el nombre de la persona a la cual corresponde dicha huella
-    
      try {
             
             if(id_e==0){
                 JOptionPane.showMessageDialog(null, "Bienvenido, "+nombre+" recuerda que para registarte debes seleccionar un evento","Verificacion de Huella", JOptionPane.INFORMATION_MESSAGE);
+                
             }else{
                 Connection c=con.conectar(); //establece la conexion con la BD
-            PreparedStatement guardarStmt = c.prepareStatement("UPDATE participantes SET estado_registro = ? WHERE documento = ? ");
-
-
-
-            guardarStmt.setString(1, "verificado");
-            //guardarStmt.setTimestamp(2, new java.sql.Timestamp(System.currentTimeMillis()));
-            guardarStmt.setInt(2, id_u);
-
-            //Ejecuta la sentencia
-            guardarStmt.execute();
-
-
-            guardarStmt.close();
-
-            //JOptionPane.showMessageDialog(null,"Huella Guardada Correctamente");
-            con.desconectar();
+                PreparedStatement guardarStmt = c.prepareStatement("UPDATE participantes SET estado_registro = ? WHERE documento = ? ");
+                guardarStmt.setString(1, "verificado");
+                //guardarStmt.setTimestamp(2, new java.sql.Timestamp(System.currentTimeMillis()));
+                guardarStmt.setInt(2, id_u);
+                //Ejecuta la sentencia
+                guardarStmt.execute();
+                guardarStmt.close();
+                //JOptionPane.showMessageDialog(null,"Huella Guardada Correctamente");
+                con.desconectar();
                 Connection c2=con.conectar();
                 //Obtiene la plantilla correspondiente a la persona indicada
                 PreparedStatement verificarStmt2 = c2.prepareStatement("SELECT id FROM detalle_participantes WHERE user_id = ? AND event_id = ?  ");     
@@ -188,57 +153,39 @@ public class VerificationForm extends CaptureForm
                 while(rs2.next()){
                     id_ex= rs2.getInt("id");
                     
-                }
-                
+                }                
                 con.desconectar();
                 if(id_ex==0){
                         Connection cc=con.conectar(); //establece la conexion con la BD
-                       PreparedStatement guardarStmt2 = cc.prepareStatement("INSERT INTO detalle_participantes (user_id , event_id ,created_at ,updated_at) VALUES(?,?,?,?)" );
-
-
-
-
-                       guardarStmt2.setInt(1,id_u);
-                       guardarStmt2.setInt(2, id_e);
-                       guardarStmt2.setTimestamp(3, new java.sql.Timestamp(System.currentTimeMillis()) );
-                       guardarStmt2.setTimestamp(4, new java.sql.Timestamp(System.currentTimeMillis()));
-
-                       //Ejecuta la sentencia
-                       guardarStmt2.execute();
-
-
-                       guardarStmt.close();
-
-                       //JOptionPane.showMessageDialog(null,"Huella Guardada Correctamente");
-                       con.desconectar();
-
-                       JOptionPane.showMessageDialog(null, "Bienvenido "+nombre,"Verificacion de Huella", JOptionPane.INFORMATION_MESSAGE);
+                        PreparedStatement guardarStmt2 = cc.prepareStatement("INSERT INTO detalle_participantes (user_id , event_id ,created_at ,updated_at) VALUES(?,?,?,?)" );
+                        guardarStmt2.setInt(1,id_u);
+                        guardarStmt2.setInt(2, id_e);
+                        guardarStmt2.setTimestamp(3, new java.sql.Timestamp(System.currentTimeMillis()) );
+                        guardarStmt2.setTimestamp(4, new java.sql.Timestamp(System.currentTimeMillis()));
+                        //Ejecuta la sentencia
+                        guardarStmt2.execute();
+                        guardarStmt.close();
+                        //JOptionPane.showMessageDialog(null,"Huella Guardada Correctamente");
+                        con.desconectar();
+                        JOptionPane.showMessageDialog(null, "Bienvenido "+nombre,"Verificacion de Huella", JOptionPane.INFORMATION_MESSAGE);
                 }else{
                         JOptionPane.showMessageDialog(null, nombre+", ya te habias registrado a este evento","Verificacion de Huella", JOptionPane.INFORMATION_MESSAGE);
                 }
-            }
-            //Establece los valores para la sentencia SQL
-    
-     
-                           
-                
-     
-     
-     
-     
-     //btnGuardar.setEnabled(false);
-     //btnVerificar.grabFocus();
+            }     
      }catch(MySQLDataException exm){
          System.err.println("Error al guardar dato los datos de la huella."+ exm.getMessage());
+         FileManager fl= new FileManager();
+         fl.Escribir("MySQLDataException: "+ exm.getMessage());   
+         JOptionPane.showMessageDialog(null,"Se ha generado un error por favor intenta la verificación de nuevo, si la falla persiste comunicate con el administrador de el sistema","Registro de Huella", JOptionPane.INFORMATION_MESSAGE);
      }
      catch (SQLException ex) {
      //Si ocurre un error lo indica en la consola
-     System.err.println("Error al guardar los datos de la huella."+ ex.getMessage());
+        System.err.println("Error al guardar los datos de la huella."+ ex.getMessage());
+        FileManager fl= new FileManager();
+        fl.Escribir("SQLException: "+ ex.getMessage());  
+        JOptionPane.showMessageDialog(null,"Se ha generado un error por favor intenta la verificación de nuevo, si la falla persiste comunicate con el administrador de el sistema","Registro de Huella", JOptionPane.INFORMATION_MESSAGE);
      }finally{
-     con.desconectar();
+        con.desconectar();
      }
    }
-    
-
-    
 }
