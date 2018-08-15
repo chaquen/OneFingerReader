24d713c1492d94a8c4f8f0484cbcad13da97353f
Adrian Guzman <edgar.guzman21@gmail.com>
2018-08-14 19:56:35 -0500

    Se elimina funcion innecesarias

--- src/com/digitalpersona/onetouch/ui/swing/sample/Enrollment/VerificationForm.java
+++ src/com/digitalpersona/onetouch/ui/swing/sample/Enrollment/VerificationForm.java
@@ -239,20 +239,6 @@ public class VerificationForm extends CaptureForm
      }
    }
     
-   public Date sumarRestarDiasFecha(Date fecha, int dias){	
- 
-	
-      Calendar calendar = Calendar.getInstance();
-	
-      calendar.setTime(fecha); // Configuramos la fecha que se recibe
-	
-      calendar.add(Calendar.DAY_OF_YEAR, dias);  // numero de días a añadir, o restar en caso de días<0
-	
- 	
-      return calendar.getTime(); // Devuelve el objeto Date con los nuevos días añadidos
-	
- 
-	
- }
+
     
 }
